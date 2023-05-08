705b5ddd7174850e6d833651da742a45fa7104dd
NtapeKnoxSiwale <ntape.k.siwale@gmail.com>
2023-05-08 20:03:37 +0200

    Reading RTDB data

--- app/src/main/java/dev/knox/cerberus/MainActivity.java
+++ app/src/main/java/dev/knox/cerberus/MainActivity.java
@@ -2,6 +2,7 @@ package dev.knox.cerberus;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 import android.widget.TextView;
 
@@ -11,39 +12,62 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.text.DecimalFormat;
+
 public class MainActivity extends AppCompatActivity {
 
-    TextView mMockDataTextVeiw;
-
-    DatabaseReference mRootRef = FirebaseDatabase.getInstance().getReference();
-    DatabaseReference mMockDataRef = mRootRef.child("Mock Data");
+    private TextView room1WeightTextView, room2WeightTextView;
+    private DatabaseReference room1Ref, room2Ref;
+    private DecimalFormat decimalFormat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // UI Data ELement
-        mMockDataTextVeiw = (TextView) findViewById(R.id.mockData);
+        // Initialize UI elements
+        room1WeightTextView = findViewById(R.id.room1_weight);
+        room2WeightTextView = findViewById(R.id.room2_weight);
 
-    }
+        // Initialize Firebase database references
+        DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference();
+        room1Ref = rootRef.child("Room1");
+        room2Ref = rootRef.child("Room2");
 
-    @Override
-    protected void onStart() {
-        super.onStart();
+        // Initialize Decimal Formatter for weight format
+        decimalFormat = new DecimalFormat("0.00");
 
-        mMockDataRef.addValueEventListener(new ValueEventListener() {
+        // Attach event listeners to database references
+        room1Ref.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                String text = snapshot.getValue(String.class);
-                mMockDataTextVeiw.setText(text);
+                Double weight = snapshot.getValue(Double.class);
+                if (weight != null) {
+                    String weightText = decimalFormat.format(weight) + " kg";
+                    room1WeightTextView.setText(weightText);
+                }
             }
 
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
-
+                // Handle error
             }
         });
 
+        room2Ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                Double weight = snapshot.getValue(Double.class);
+                if (weight != null) {
+                    String weightText = decimalFormat.format(weight) + " kg";
+                    room2WeightTextView.setText(weightText);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                // Handle error
+            }
+        });
     }
 }
